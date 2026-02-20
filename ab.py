99aa82ae67c2b629c56a58dab5175c85b7b36265
mangalji <rajmangal03@gmail.com>
2026-02-20 22:59:51 +0530

    unknown

--- ab.py
+++ ab.py
@@ -1,3 +1,10 @@
+
+# import calendar
+# year = 2026
+# month = 4
+# print(calendar.month(year,month))
+
+
 import calendar
 
 print("Calender Program")
@@ -5,8 +12,4 @@ year = int(input("enter the year: "))
 month = int(input("enter the month in digits(1-12): "))
 
 print("\nHere is the calendar: \n")
-try:
-	print(calendar.month(year,month))
-except IndexError:
-	print("you are given wrong input!!!")
-	print("please enter correct input next time...")
+print(calendar.month(year,month))
